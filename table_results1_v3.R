20abc03064943388809d598c8fdbf93f00a05d8b
Raphaël Huleux <51329410+raphaelhuleux@users.noreply.github.com>
2020-06-05 10:29:38 +0200

    Adding GATEs and Naive OLS to the main file

--- table_results1_v3.R
+++ table_results1_v3.R
@@ -1,30 +1,18 @@
 rm(list = ls())
 
 # Installs packages if not already installed, then loads packages 
-list.of.packages <- c("glmnet", "rpart.plot", "randomForest", "devtools", "tidyverse", "knitr", "SuperLearner", "caret", "xgboost")
+list.of.packages <- c("doBy", "lfe", "dplyr", "readstata13", "causalTree", "githubinstall", "rpart.plot", "devtools", "base", "dummies", "sjlabelled", "glmnet", "rpart.plot", "randomForest", "devtools", "tidyverse", "knitr", "SuperLearner", "caret", "xgboost")
 new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
 if(length(new.packages)) install.packages(new.packages, repos = "http://cran.us.r-project.org")
 invisible(lapply(list.of.packages, library, character.only = TRUE))
-library(sjlabelled)
-library(dummies)
-library(base)
-library(devtools) 
-library(rpart.plot)
-library(githubinstall)
-library(causalTree)
-library(readstata13)
-library(dplyr)
-library(lfe)
-library(doBy)
 
 tables_stlima <- read.dta13("C:/Users/Carlos Leon/Desktop/PSE/Second semester/Machine Learning/final_project/tables_stlima.dta")
-
 
 #######################################################
 ################   Table 2   ##########################
 #######################################################
 
-#Keep relevant observations
+# Keep relevant observations
 table_2 <- subset(tables_stlima, follow_up==1)
 table_2 <- subset(table_2, grado_r1>=3 & grado_r1<=6)
 table_2 <- subset(table_2, !is.na(male_r2) & !is.na(age_r2) & !is.na(sibling_r2) & 
@@ -39,7 +27,6 @@ table_2 <- subset(table_2, !is.na(male_r2) & !is.na(age_r2) &
 table_2 <- subset(table_2, participated_in_lottery==1)
 table_2 <- subset(table_2, !is.na(pchome_r1) & !is.na(pchome_r2))
 
-
 table_2 <- mutate(table_2, tcol= ifelse(participated_in_lottery==1 & won_lottery==1,1,0))
 
 
@@ -52,7 +39,7 @@ table_2 <- mutate(table_2, cfixeff=group_indices(table_2,cfixeff_key))
 table_2 <- mutate(table_2, cfixeff=as.factor(cfixeff))
 table_2 <- mutate(table_2, codmod=as.factor(codmod))
 
-#Column 3
+# Column 3
 
 dependent <- c("pchome", "pcuweek", "pcuyest", "pcuwsch", "pcuwhome", 
                "pcuwcafe", "pcuwfri", "pcuytwsch", "pcuytwhome", 
@@ -87,6 +74,7 @@ print(Table2_C3)
 ####################################################### 
 ################   Table 8   ##########################
 #######################################################
+
 table_8 <- read.dta13("C:/Users/Carlos Leon/Desktop/PSE/Second semester/Machine Learning/final_project/One_laptopfiles/data/data_table_8.dta")
 table_8 <- mutate(table_8, cfixeff_key = paste(table_8$codmod,
                                                table_8$grado_r1, table_8$seccion_r1,sep=""))
@@ -181,12 +169,36 @@ colnames(Table8) <- c("Dep.var.", "Males", "Females", "N",
 
 print(Table8)
 
+###############################################
+################   NAIVE OLS   ################   
+###############################################
+
+CT_dat <- read.dta13("/Users/raphaelhuleux/Documents/GitHub/machine_learning_project/Data/data_table_8.dta")
+
+dependent1 <- c("stxo", "stpcotskill", "stpcsrskill", "straven")
+df <- subset(CT_dat, select = -c(pcuytwothr_r2, pcuwothr_r2, codest, codmod, nombr_es))
+df <- na.omit(df)
+Y <- df[, names(df) %in% paste(dependent1,"r2",sep = "_")]
+X <- df[, names(df) %in% "tcol"]
+W <- df[, names(df) %in% 
+              grep("r1",names(df),fixed = TRUE, value = TRUE)]
+W$g34  <- df$g34
+W$male <- ifelse(df$female!=1,1,0)
+
+sl_lm <- list()
+for(i in 1:length(dependent1)){sl_lm[[i]] = SuperLearner(Y = Y[i], 
+                     X = data.frame(X=X, W, W*X), 
+                     family = gaussian(), 
+                     SL.library = "SL.lm", 
+                     cvControl = list(V=0))}
+
+summary(sl_lm$fitLibrary$SL.lm_All$object)
+
 ####################################################### 
 ################   Causal Trees   #####################
 #######################################################
-CT_dat <- read.dta13("C:/Users/Carlos Leon/Desktop/PSE/Second semester/Machine Learning/final_project/One_laptopfiles/data/data_table_8.dta")
 
-dependent1 <- c("stxo", "stpcotskill", "stpcsrskill", "straven")
+
 
 for (i in 1:length(dependent1)){
 set.seed(1234)
@@ -251,7 +263,7 @@ leaf2 <- as.factor(round(predict(honest_tree_prune,
 honest_ols_2 <- lm( Y ~ leaf + X * leaf - X -1, data = data.frame(Y=Y2, X=X2, leaf=leaf2, W2))
 summary(honest_ols_2) 
 
-#CATE HT
+# CATE HT
 cate_honesttree <- predict(honest_tree_prune, newdata = data.frame(Y=Y2, W2), type = "vector")
 
 
@@ -281,24 +293,25 @@ causalforest <- causalForest(CT_fml,
                              ncov_sample = ncol(W1), 
                              ncolx = ncol(W1))
 
-#CATE 
+# CATE 
 cate_causalforest <- predict(causalforest, newdata=data.frame(Y=Y2, W2), type="vector")
 
 #####################################################################
 ########################   Graphs   #################################
 #####################################################################
 
+#####################################################################
 ##############  Histograms Causal Trees / Forest  ###################
 #####################################################################
 
-#Plot in R
+# Plot in R
 par(mfrow = c(1, 2))
 hist(cate_honesttree, main = paste("HCT",dependent1[i],sep = " - "), 
      col="black")
 hist(cate_causalforest, main = paste("HCF",dependent1[i],sep = " - "),
      col="blue")
 
-#Save the plot in working directory
+# Save the plot in working directory
 
 png(paste(dependent1[i],"CTCF.png",sep = "_"))
 par(mfrow = c(1, 2))
@@ -308,10 +321,10 @@ hist(cate_causalforest, main = paste("HCF",dependent1[i],sep = " - "),
      col="blue")
 dev.off()
 
-#Reset Plot conf.
+# Reset Plot conf.
 par(mfrow = c(1, 1))
 
-
+#####################################################################
 ##################  CATE Quantile Causal Forest   ###################
 #####################################################################
 
@@ -329,8 +342,9 @@ s <- cf_quintile %>%
   summarize_all(~sqrt(var(.,na.rm = TRUE)/length(.)), na.rm = TRUE) %>% 
   gather(key="variable", value = "value", -cate_quintile)
 
-##### Variables reported in the paper
-########################################
+###########################################
+##### Variables reported in the paper #####
+###########################################
 
 m <- as.data.frame(m)
 var_paper  <- c("male","g34","pchome_r1", "bmavmr_r1", "cate")
@@ -349,8 +363,10 @@ m_paper %>% ggplot(aes(x=factor(cate_quintile), y=value)) +
 ggsave(paste(dependent1[i],"report_paper.png",sep="_"), device="png",  plot = last_plot(),
        path = "./")
 
-##### Variables not reported in the paper
-#########################################
+
+#############################################
+##### Variables not reported in the paper ###
+#############################################
 
 m_notpaper <- m[!m$variable %in% var_paper ,]
 s_notpaper <- s[!s$variable %in% var_paper ,]
@@ -370,15 +386,112 @@ ggsave(paste(dependent1[i],j,"not_report_paper.png",sep="_"), device="png",  plo
 }
 
 par(mfrow = c(1, 1))
-
+}
 
 ####################################################### 
 ################      GATES       #####################
 #######################################################
 
-
-
+gates <- function(Y, W, X, Q=4, prop_scores=F) {
+  
+  ### STEP 1: split the dataset into two sets, 1 and 2 (50/50)
+  split <- createFolds(1:length(Y), k=2)[[1]]
+  
+  Ya = Y[split]
+  Yb = Y[-split]
+  
+  Xa = X[split]
+  Xb = X[-split]
+  
+  Wa = W[split, ]
+  Wb = W[-split, ]
+  
+  ### STEP 2a: (Propensity score) On set A, train a model to predict X using W. Predict on set B.
+  if (prop_scores==T) {
+    sl_w1 = SuperLearner(Y = Xa, 
+                         X = Wa, 
+                         newX = Wb, 
+                         family = binomial(), 
+                         SL.library = "SL.xgboost", 
+                         cvControl = list(V=0))
+    
+    p <- sl_w1$SL.predict
+  } else {
+    p <- rep(mean(Xa), length(Xb))
+  }
+  
+  ### STEP 2b let D = W(set 😎 - propensity score.
+  D <- Xb-p
+  
+  ### STEP 3a: Get CATE (for example using causal forests) on set A. Predict on set B.
+  tree_fml <- as.formula(paste("Y", paste(names(Wa), collapse = ' + '), sep = " ~ "))
+  cf <- causalForest(tree_fml,
+                     data=data.frame(Y=Ya, Wa), 
+                     treatment=Xa, 
+                     split.Rule="CT", 
+                     split.Honest=T,  
+                     split.Bucket=T, 
+                     bucketNum = 5,
+                     bucketMax = 100, 
+                     cv.option="CT", 
+                     cv.Honest=T, 
+                     minsize = 2, 
+                     split.alpha = 0.5, 
+                     cv.alpha = 0.5,
+                     sample.size.total = floor(nrow(Wa) / 2), 
+                     sample.size.train.frac = .5,
+                     mtry = ceiling(ncol(W1)/3), 
+                     nodesize = 5, 
+                     num.trees = 10, 
+                     ncov_sample = ncol(Wa), 
+                     ncolx = ncol(Wa))
+  
+  cate_cf <- predict(cf, newdata = Wb, type="vector")
+  
+  ### STEP 3b: divide the cate estimates into Q tiles, and call this object G. 
+  # Divide observations into n tiles
+  G <- data.frame(cate_cf) %>% # replace cate_cf with the name of your predictions object
+    ntile(Q) %>%  # Divide observations into Q-tiles
+    factor()
+  
+  ### STEP 4: Create a dataframe with Y, W (set B), D and G. Regress Y on group membership variables and covariates. 
+  df <- data.frame(Y=Yb, Wb, D, G, p)
+  
+  Wnames <- paste(colnames(Wb), collapse="+")
+  fml <- paste("Y ~",Wnames,"+ D:G")
+  model <- lm(fml, df, weights = 1/(p*(1-p))) 
+  
+  return(model) 
 }
 
+# Turn the GATES into a nice table:
+
+table_from_gates <-function(model) {
+  thetahat <- model%>% 
+    .$coefficients %>%
+    .[c("D:G1","D:G2","D:G3","D:G4")]
+  
+  # Confidence intervals
+  cihat <- confint(model)[c("D:G1","D:G2","D:G3","D:G4"),]
+  
+  res <- tibble(coefficient = c("gamma1","gamma2","gamma3","gamma4"),
+                estimates = thetahat,
+                ci_lower_90 = cihat[,1],
+                ci_upper_90 = cihat[,2])
+  
+  return(res)
+}
+
+# Repeat for inference:
+
+output <- list()
+
+for (i in 1:length(dependent1)){
+  output[[i]] <- rerun(10, table_from_gates(gates(Y[,i], W, X))) %>% # Increase reruns in practice!
+    bind_rows %>%
+    group_by(coefficient) %>%
+    summarize_all(median)
+}
+output
 
  
